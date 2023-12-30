147f1e80d0888c3858df2449300f7511e1b70baa
MerakiShubh <shubhamt.champ@gmail.com>
2023-12-30 09:22:44 +0530

    Multer Configuration

--- backend/routes/files.js
+++ backend/routes/files.js
@@ -1,6 +1,27 @@
 import { Router } from "express";
+import { nanoid } from "nanoid";
+import path from "path";
 
+import multer from "multer";
 const router = Router();
+
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cg(null, "../uploads");
+  },
+  filename: (req, file, cb) => {
+    // const uniqueName = `${Date.now()} + "-" + ${Math.round(
+    //   Math.random() * 1e9
+    // )}${path.extname(file.originalname)}`;
+    const uniqueName = nanoid() + path.extname(file.originalname);
+    cb(null, uniqueName);
+  },
+});
+
+let upload = multer({
+  storage,
+  limit: { fileSize: 100000 * 100 },
+}).single("myfile");
 
 router.post("/", (req, res) => {
   //validate request
