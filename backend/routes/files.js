2a63e9096715577e4c3450defd9482cda43b91c3
MerakiShubh <shubhamt.champ@gmail.com>
2023-12-30 23:52:19 +0530

    Bug resolved in .env by removing / which is at the last of MONGODB_URI

--- backend/routes/files.js
+++ backend/routes/files.js
@@ -1,40 +1,54 @@
 import { Router } from "express";
-import { nanoid } from "nanoid";
+// import { nanoid } from "nanoid";
+import { v4 as uuidv4 } from "uuid";
 import path from "path";
-
+import { File } from "../models/file.model.js";
 import multer from "multer";
 const router = Router();
 
 let storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cg(null, "../uploads");
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
-    // const uniqueName = `${Date.now()} + "-" + ${Math.round(
-    //   Math.random() * 1e9
-    // )}${path.extname(file.originalname)}`;
-    const uniqueName = nanoid() + path.extname(file.originalname);
+    const uniqueName = `${Date.now()}-${Math.round(
+      Math.random() * 1e9
+    )}${path.extname(file.originalname)}`;
+    // const uniqueName = nanoid() + path.extname(file.originalname);
     cb(null, uniqueName);
   },
 });
 
 let upload = multer({
   storage,
-  limit: { fileSize: 100000 * 100 },
+  limits: { fileSize: 100000 * 100 },
 }).single("myfile");
 
 router.post("/", (req, res) => {
-  //validate request
-
-  if (!req.file) {
-    return res.json({ error: "All fields are required" });
-  }
-
   //store files
+  upload(req, res, async (err) => {
+    //validate request
+    if (!req.file) {
+      return res.json({ error: "All fields are required" });
+    }
+    if (err) {
+      return res.status(500).send({ error: err.message });
+    }
+    //store into database
+    const file = new File({
+      filename: req.file.filename,
+      uuid: uuidv4(),
+      path: req.file.path,
+      size: req.file.size,
+    });
 
-  //store into database
+    const response = await file.save();
 
-  //Response -> link
+    return res.json({
+      file: `${process.env.APP_BASE_URL}/files/${response.uuid}`,
+    });
+    //http://localhost:3000/files/235884-3hsdfh
+  });
 });
 
 export default router;
