5d5c870d94ef949e60c09952686ba0f73c94ffb3
MerakiShubh <shubhamt.champ@gmail.com>
2023-12-30 09:25:19 +0530

    Mongoose Schema Defined

---
+++ backend/models/file.model.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const fileSchema = new Schema(
+  {
+    fileName: { type: String, required: true },
+    path: { type: String, required: true },
+    size: { type: Number, required: true },
+    uuid: { type: String, required: true },
+    sender: { type: String, required: false },
+    receiver: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("File", fileSchema);
