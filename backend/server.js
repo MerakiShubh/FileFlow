0d1f4f0db2bbabdbfdc8e99a3366f58ea4978aec
MerakiShubh <shubhamt.champ@gmail.com>
2023-12-31 23:46:55 +0530

    Configure Express Templete Engine

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import router from "./routes/files.js";
+import router1 from "./routes/show.js";
 import { app } from "./app.js";
+import path from "path";
 
 dotenv.config({
   path: "./env",
@@ -17,6 +19,21 @@ connectDB()
     console.log("Mongo db connection failed!!", err);
   });
 
+// <---------------------------Templeting Engine Trial ----------------------------------->
+
+// app.set("views", path.join(__dirname, "/views"));
+// app.set("views", "ejs");
+// const viewsPath = path.join(import.meta.url, "/views");
+// app.set("views", viewsPath);
+// app.set("view engine", "ejs");
+// const viewsPath = path.join(new URL(".", import.meta.url).pathname, "/views");
+// app.set("views", viewsPath);
+
+//Template Engine
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
 //Routes
 
 app.use("/api/files", router);
+app.use("/files", router1);
