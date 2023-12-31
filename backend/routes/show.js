829c0e0b80bb72f1bee431b8f73d10074b11fd50
MerakiShubh <shubhamt.champ@gmail.com>
2023-12-31 23:53:19 +0530

    Created new router/controller to view files on frontend

---
+++ backend/routes/show.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import { File } from "../models/file.model.js";
+
+const router1 = Router();
+
+router1.get("/:uuid", async (req, res) => {
+  try {
+    const fileFetch = await File.findOne({
+      uuid: req.params.uuid,
+    });
+    if (!fileFetch) {
+      return res.render("download", { error: "Link has been expired!!!" });
+    }
+
+    return res.render("download", {
+      uuid: fileFetch.uuid,
+      fileName: fileFetch.filename,
+      fileSize: fileFetch.size,
+      download: `${APP_BASE_URL}/files/download/${fileFetch.uuid}`,
+    });
+  } catch (err) {
+    return res.render("download", { error: "Something went wrong!!!" });
+  }
+});
+
+export default router1;
